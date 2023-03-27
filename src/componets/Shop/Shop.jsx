be456c114afb5639694670f57bb8d89e34b194b2
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-03-28 02:58:05 +0600

    local storage activity

--- src/componets/Shop/Shop.jsx
+++ src/componets/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -13,10 +14,26 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+useEffect(()=>{
+const storedCart =  getShoppingCart()
+for(const id in storedCart){
+ const addedProduct= products.find(product => product.id === id)
+ console.log(addedProduct)
+ const quantity = storedCart[id]
+ addedProduct.quantity = quantity;
+}
+}, [products])
+
+
   const handleCartItem = (product) => {
     const newProduct = [...cart, product];
     setCart(newProduct);
+    addToDb(product.id)
   };
+
+
+
+
   return (
     <div className="shop-container">
       <div className="product-container">
